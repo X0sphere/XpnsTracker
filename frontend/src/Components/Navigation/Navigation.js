00aee823659563f7ac5ba6b30c57a0061fc4b1c6
X0sphere <118646588+X0sphere@users.noreply.github.com>
2024-01-27 21:03:48 +0530

    added appJs and other nav

---
+++ frontend/src/Components/Navigation/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Navigation() {
+  return (
+    <NavStyled>
+        <div className='user-connec'>
+            
+        </div>
+
+
+
+
+    </NavStyled>
+  )
+}
+
+const NavStyled = styled.nav`
+
+`;
+export default Navigation
